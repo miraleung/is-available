34046bda5c4bf1554ff412a003aad8a0094b5a63
mira-leung <lngm1@hotmail.com>
2013-07-16 15:05:58 -0700

    Initial push

---
+++ js/is_available.js
@@ -0,0 +1,13 @@
+$(function () {
+	$(document).ready(function() {
+		alert("JAVASCRIPT LOADED!");
+		/* $('div.cal-popup-start').blur(function() {
+			alert("YAY!");
+		});
+		
+		/* Change the labels of the time fields 
+		$("div.form-item-cal-popup-start-time.label").attr("for").html("Start Time");
+		$("div.form-item-cal-popup-end-time.label").attr("for").html("Start Time");
+		 */
+	});
+})(jQuery);
