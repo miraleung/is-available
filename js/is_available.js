bfca7e85fc5e4c8e2060813a21126ff2018646f3
quicksort <quicksort@2583672.no-reply.drupal.org>
2013-08-06 17:05:19 -0700

    Theming and form element changes.

--- js/is_available.js
+++ js/is_available.js
@@ -1,9 +1,45 @@
 $(function () {
 	$(document).ready(function() {
 		/* Admin settings form theming */
-		$('#configure-setting-button').before($('#setting-display'));
 		
-		/* Actual form theming will go here */
+$('#edit-settings-options').after($('#delete-setting-button'));
+$('#edit-settings-options').after($('#add-setting-button'));
+$('#edit-settings-options').after($('#configure-setting-button'));
+
+
+	/* Actual form theming will go here */
+$(this).on("click", '#configure-resource-type', function(event) {
+    alert("Clicked");
+});
+$(this).on("click", '#configure-building', function(event) {
+    alert("Clicked");
+});
+
+
+$('input.form-text.hasTimeEntry').css('width', '100% !important');
+
+$('.form-item-cal-popup-start-date > label').hide();
+$('.form-item-cal-popup-start-date:first-child').css('margin-top', '2px');
+
+
+$('input#edit-cal-popup-start-datepicker-popup-0').focusout(function() {
+    alert('asdfsadfsdfa');
+});
+$("input[id^='edit-cal-popup-start-']").focusout(function() {
+    var parent = $(this).parent().parent().parent().parent().parent().parent();
+// alert(parent.attr('class'));
+    var next = parent.next();
+   var end = next.find('span.timeEntry_wrap'); //"input[id^='edit-cal-popup-start-']");
+    alert(end.attr('class'));
+    alert(end.val());    
+});
+
+
+
+$('.form-item-cal-popup-end-date > label').hide();
+$('.form-item-cal-popup-end-date:first-child').css('margin-top', '2px');
+		
+		
 		
 	});
 })(jQuery);
